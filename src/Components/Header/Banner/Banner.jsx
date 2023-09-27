6664e839e28d2c5dcff2b10179a9a988fbbbcec2
mdriazuddinriad5 <mdriazuddinriad5@gmail.com>
2023-09-27 18:00:04 +0600

    Search option inside Home implemented

--- src/Components/Header/Banner/Banner.jsx
+++ src/Components/Header/Banner/Banner.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
 
 
-const Banner = () => {
+const Banner = ({handleSearch}) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const handleCategory=()=>{
+        handleSearch(inputValue)
+        setInputValue('')
+    }
+
 
     return (
         <div>
@@ -17,8 +26,14 @@ const Banner = () => {
                 <div className="hero-content text-center text-neutral-content relative z-10">
                     <div>
                         <h1 className="mb-10 text-5xl font-bold">I Grow By Helping People In Need</h1>
-                        <input type="text" placeholder="Search here" className="input input-bordered border-[#DEDEDE] input-primary w-full max-w-xs focus:border-none" />
-                        <button className="py-3 px-6 rounded-lg bg-[#FF444A] border-none text-[#FFFFFFF2]">Search</button>
+                        <input 
+                        type="text" 
+                        placeholder="Search here" 
+                        className="input input-bordered border-[#DEDEDE] input-primary w-full max-w-xs focus:border-none"
+                        value={inputValue}
+                        onChange={(e)=> setInputValue(e.target.value)}
+                        />
+                        <button onClick={handleCategory} className="py-3 px-6 rounded-lg bg-[#FF444A] border-none text-[#FFFFFFF2]">Search</button>
                     </div>
                 </div>
             </div>
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import Donations from "../../Components/Donations/Donations";
 import Banner from "../../Components/Header/Banner/Banner";
-import {useLoaderData} from "react-router-dom"
+import { useLoaderData } from "react-router-dom"
+
 
 const Home = () => {
 
-    const donations= useLoaderData();
+    const [filteredDonations, setFilteredDonation] = useState([]);
+
+    const donations = useLoaderData();
+
+    const handleSearch = inputVal => {
+
+        console.log(donations);
+        console.log(inputVal)
+        const filteredData = donations.filter(donation => donation.category.toLowerCase() === inputVal.toLowerCase())
+        console.log(filteredData);
+
+        if (filteredData) {
+            setFilteredDonation(filteredData);
+        }
+    }
 
     return (
         <div>
-            <Banner></Banner>
-            <Donations donations={donations}></Donations>
+            <Banner handleSearch={handleSearch}></Banner>
+            <Donations donations={filteredDonations.length > 0 ? filteredDonations : donations}></Donations>
         </div>
     );
 };
